86d25f6d2bd626020165d44d48cc5f0cb645d24d
Ryan Robby <147597256+RyanRyzer@users.noreply.github.com>
2025-01-25 18:17:46 +0700

    Update script.js

--- Final Project/script.js
+++ Final Project/script.js
@@ -36,9 +36,31 @@ elements.forEach(el => observer.observe(el));
 
 window.addEventListener('scroll', function() {
     const navbar = document.querySelector('header.navbar-container');
-    if (window.scrollY > 50) { // Ganti 50 dengan nilai scroll yang diinginkan
+    if (window.scrollY > 50) {
         navbar.classList.add('scrolled');
     } else {
         navbar.classList.remove('scrolled');
     }
 });
+
+document.getElementById('search-button').addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log('Tombol cari diklik');
+    const searchInput = document.getElementById('search-input').value.toLowerCase();
+    console.log('Kata kunci pencarian:', searchInput);
+    const elementsToSearch = document.querySelectorAll('.anime-item');
+
+    elementsToSearch.forEach(element => {
+        if (element.textContent.toLowerCase().includes(searchInput)) {
+            element.style.display = 'block';
+        } else {
+            element.style.display = 'none';
+        }
+    });
+});
+
+function showThankYouMessage() {
+    alert("Terima kasih telah mendukung kami");
+    window.location.href = "index.html";
+    return false;
+}
